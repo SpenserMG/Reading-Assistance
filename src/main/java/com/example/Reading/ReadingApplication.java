df9f2efe6ac4dfd784d6d8176978fc57b33c16c5
SpenserMG <spensermgreeley@gmail.com>
2022-06-16 09:41:46 -0400

    First Commit

---
+++ src/main/java/com/example/Reading/ReadingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Reading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Reading/ReadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Reading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
